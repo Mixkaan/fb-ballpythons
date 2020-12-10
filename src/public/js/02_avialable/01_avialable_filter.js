5fe72aa8a8b7311267e0d6d4a870308bddde4545
victor <gatitto079@gmail.com>
2020-12-09 21:24:20 -0600

    09-12-2020

--- src/public/js/02_avialable/01_avialable_filter.js
+++ src/public/js/02_avialable/01_avialable_filter.js
@@ -1,7 +1,53 @@
-const cria = document.getElementById('cria');
+// function filter() {
 
-cria.addEventListener('click', function() {
-  const age = document.getElementById('age');
-  age.filter( age = 'cria');
-  console.log(age)
-})
+//   //- CARD-SELECTION
+//   var card = document.getElementById('cardSelector');
+//   var indicator = document.getElementById('indicator')
+//   //- CARD-PROPERTIES
+//   var age = card.dataset.age;
+//   var gender = card.dataset.gender;
+//   var price = card.dataset.price;
+
+//   //- BUTTON-ACTION
+//   var filterButton = document.getElementById('filterButton');
+
+//   // filterButton.addEventListener('click', () => {
+//   //   //- GET SELECT VALUE
+//   //   var filterSelectAge = document.getElementById('filterSelectAge').value;
+
+//   //   if (filterSelectAge === 'cria') {
+      
+//   //     for (var i = 0; i < card.length; i++) {
+//   //       card.classList.add('desactive');
+//   //       console.log(card);
+        
+//   //     }
+//   //     console.log(card)
+//   //     // card.classList.add('desactive');
+//   //   } else {
+//   //   }
+//   // });
+// }
+// filter();
+
+
+//- CARD-SELECTION
+var card = document.getElementById('cardSelector');
+var indicator = document.getElementById('indicator')
+//- CARD-PROPERTIES
+var age = card.dataset.age;
+var gender = card.dataset.gender;
+var price = card.dataset.price;
+
+//- BUTTON-ACTION
+var filterButton = document.getElementById('filterButton');
+
+function test() {
+  for (let i = 0; i < card.length; i++) {
+    const element = card[i];
+    console.log(element)
+    element.classList.add('desactive')
+  }
+  
+}
+test();
